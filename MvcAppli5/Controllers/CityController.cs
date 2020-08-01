47feac6683e32ece794325ad4ec69f6ee24f9ae0
Rishi <suryawanshirishi.nmims@gmail.com>
2020-08-01 16:43:31 +0530

    MvcAppli5

---
+++ MvcAppli5/Controllers/CityController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Web.Mvc;
+using MvcAppli5.Models;
+
+namespace MvcAppli5.Controllers
+{
+    public class CityController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            SampleDBContext db = new SampleDBContext();
+            return View(db.Cities);
+        }
+
+        [HttpPost]
+        public string Index(IEnumerable<City> cities)
+        {
+            if (cities.Count(x => x.IsSelected) == 0)
+            {
+                return "You have not selected any City";
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("You selected - ");
+                foreach (City city in cities)
+                {
+                    if (city.IsSelected)
+                    {
+                        sb.Append(city.Name + ", ");
+                    }
+                }
+                sb.Remove(sb.ToString().LastIndexOf(","), 1);
+                return sb.ToString();
+            }
+        }
+
+    }
+}
---
+++ MvcAppli5/Models/Company.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcAppli5.Models
+{
+    public class Company
+    {
+        public string SelectedDepartment { get; set; }
+        public List<Department> Departments
+        {
+            get
+            {
+                hdfcEntities db = new hdfcEntities();
+                return db.Departments.ToList();
+            }
+        }
+    }
+}
